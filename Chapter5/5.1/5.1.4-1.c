8e1c46ac55571ef09bc45b9ffde743fbccd5c493
panda-lsy <shengxia23@hainnu.edu.cn>
2025-04-27 15:15:11 +0800

    更新第5章示例代码，添加选择排序和插入排序的实现及相关注释

--- Chapter5/5.1/5.1.4-1.c
+++ Chapter5/5.1/5.1.4-1.c
@@ -2,25 +2,35 @@
 
 int main(){
     int n;
-    scanf("%d",&n);
-    int a[n];
+    scanf("%d",&n);  // 读取数组的大小
+    int a[n];        // 声明一个大小为n的数组
+    
+    // 读取n个整数到数组中
     for (int i=0;i<n;i++){
         scanf("%d",&a[i]);
     }
+    
+    // 使用选择排序算法对数组进行排序
     for (int i=0;i<n-1;i++){
-        int min_index=i;
+        int min_index=i;  // 假设当前位置i是最小值的索引
+        
+        // 在剩余未排序的元素中寻找最小值
         for (int j=i+1;j<n;j++){
             if (a[j]<a[min_index]){
-                min_index=j;
+                min_index=j;  // 更新最小值的索引
             }
         }
+        
+        // 交换当前位置i与最小值位置min_index的元素
         int temp;
         temp=a[min_index];
         a[min_index]=a[i];
         a[i]=temp;
     }
+    
+    // 打印排序后的数组
     for (int i=0;i<n;i++){
         printf("%d ",a[i]);
     }
-    return 0;
+    return 0;  // 程序正常结束
 }
--- Chapter5/5.1/5.1.4-2.c
+++ Chapter5/5.1/5.1.4-2.c
@@ -1,23 +1,35 @@
 #include "stdio.h"
 
 int main(){
-    int n,temp;
-    scanf("%d",&n);
+    int n, temp;
+    // 输入数组大小
+    scanf("%d", &n);
+    // 定义一个大小为n的数组
     int a[n];
-    for (int i=0;i<n;i++){
-        scanf("%d",&a[i]);
+    
+    // 循环输入n个整数到数组
+    for (int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
     }
-    for (int i=0;i<n-1;i++){
-        for (int j=0;j<n-i-1;j++){
-            if (a[j]>a[j+1]){
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
+    
+    // 冒泡排序算法实现
+    // 外层循环控制排序轮数，n个数需要n-1轮
+    for (int i = 0; i < n-1; i++){
+        // 内层循环进行相邻元素比较和交换
+        // 每轮结束后，最大的i+1个元素已排好序，所以只需比较到n-i-1
+        for (int j = 0; j < n-i-1; j++){
+            // 如果前一个元素大于后一个元素，则交换它们
+            if (a[j] > a[j+1]){
+                temp = a[j];
+                a[j] = a[j+1];
+                a[j+1] = temp;
             }
         }
     }
-    for (int i=0;i<n;i++){
-        printf("%d ",a[i]);
+    
+    // 输出排序后的数组
+    for (int i = 0; i < n; i++){
+        printf("%d ", a[i]);
     }
     return 0;
 }
